166077f43e0fa145550e8b850e1e2bab8e5483b9
n8anderson <bsuCompSci2021!>
2021-10-06 10:47:52 -0400

    SQL and Existing DB

---
+++ Insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO inventory (itemID, item_description, quantity, restock_ordered, ordered_by)
+VALUES 	(1002, "Tortilla", 500, 0, 0),
+		(1003, "Beans", 200, 0, 0),
+		(1004, "Beef", 50, 1, 100),
+		(1005, "Chicken", 10, 1, 100),
+		(1006, "Sour Cream", 6, 0, 0),
+		(1007, "Lettuce", 10, 0, 0),
+		(1008, "Tomato", 200, 0, 0),
+		(1009, "Steak", 40, 1, 102),
+		(1010, "Chips", 200, 0, 0),
+		(1011, "Salsa", 6, 1, 102),
+		(1012, "Rice", 9, 1, 102),
+		(1013, "Onion", 15, 1, 102),
+		(1014, "Spices", 8, 0, 0)
